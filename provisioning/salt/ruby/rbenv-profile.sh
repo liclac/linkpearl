fe6f5fa6de0d73b9d69d7328391441fdfa08dd87
uppfinnarn <uppfinnarn@gmail.com>
2015-09-01 16:36:09 +0000

    Allow user gem installation (oops)

--- provisioning/salt/ruby/rbenv-profile.sh
+++ provisioning/salt/ruby/rbenv-profile.sh
@@ -1,3 +1,9 @@
-export RBENV_ROOT=/usr/local/rbenv
-export PATH=$RBENV_ROOT/bin:$PATH
+# Load global rbenv into all shells
+export RBENV_ROOT="/usr/local/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init -)"
+
+# Allow user-specific gem installations
+export GEM_HOME="$HOME/.gem"
+export GEM_PATH="$GEM_HOME"
+export PATH="$GEM_PATH/bin:$PATH"
