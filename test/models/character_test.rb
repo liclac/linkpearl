cd810e7f5974619d449b33e86be47d697bf88ffb
uppfinnarn <uppfinnarn@gmail.com>
2015-09-02 23:42:58 +0000

    Tests for protected profiles too

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 EMI_HTML = File.read(Rails.root.join('test', 'data', 'emi.html'))
+YOSHIDA_HTML = File.read(Rails.root.join('test', 'data', 'yoshida.html'))
 
 class CharacterTest < ActiveSupport::TestCase
   test "parsing HTML works" do
     emi = Character.from_html EMI_HTML
     assert_equal 7248246, emi.id
     assert_equal "Emi Katapow", emi.name
+  end
+  
+  test "parsing protected profiles works" do
+    yoshi = Character.from_html YOSHIDA_HTML
+    assert_equal 7806252, yoshi.id
+    assert_equal "Yoshida Eee", yoshi.name
   end
 end
