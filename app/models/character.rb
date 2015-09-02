8dcc329360917d35ac06be3d829af23fcfa80c29
uppfinnarn <uppfinnarn@gmail.com>
2015-09-02 23:29:37 +0000

    And IDs

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,8 @@ class Character < ActiveRecord::Base
   include Parseable
   
   def parse_doc(doc)
-    self.name = doc.at_css('.player_name_txt h2 a').content
+    name_link = doc.at_css('.player_name_txt h2 a')
+    self.id = name_link['href'].split('/').last.to_i
+    self.name = name_link.content
   end
 end
--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -5,6 +5,7 @@ EMI_HTML = File.read(Rails.root.join('test', 'data', 'emi.html'))
 class CharacterTest < ActiveSupport::TestCase
   test "parsing HTML works" do
     emi = Character.from_html EMI_HTML
+    assert_equal 7248246, emi.id
     assert_equal "Emi Katapow", emi.name
   end
 end
