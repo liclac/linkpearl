3d166fff04a12621e2000a0f21eb07ea44aea10d
uppfinnarn <uppfinnarn@gmail.com>
2015-09-24 13:03:07 +0000

    Working imports

--- app/jobs/import_characters_job.rb
+++ app/jobs/import_characters_job.rb
@@ -2,8 +2,8 @@ class ImportCharactersJob < ActiveJob::Base
   queue_as :default
 
   def perform(first_id = nil, batch_size = nil)
-    first_id = first_id || Character.select(:id).where('name IS NOT NULL').order(:id).last.try(:id) || 1
-    batch_size = batch_size || Character.where('id > ?', first_id).count >= 50 ? 150 : 500
+    first_id = first_id || Character.where(:name.ne => nil).desc(:_id).first.try(:id) || 1
+    batch_size = batch_size || Character.where(:_id.gt => first_id).count >= 50 ? 150 : 500
     
     last_id = first_id + batch_size
     (first_id...last_id).each do |id|
