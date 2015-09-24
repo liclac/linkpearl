0a14ba5cdd8b671fd7e696b66b7cba12d57a04fc
uppfinnarn <uppfinnarn@gmail.com>
2015-09-24 14:13:53 +0000

    Primary/secondary stats, mainly to detect changes

--- app/models/character.rb
+++ app/models/character.rb
@@ -15,8 +15,24 @@ class Character
   include Mongoid::Timestamps
   include Parseable
   
+  # General information
   field :name, type: String
-  field :stats, type: Hash
+  
+  # Primary attributes
+  field :str, type: Integer
+  field :dex, type: Integer
+  field :vit, type: Integer
+  field :int, type: Integer
+  field :mnd, type: Integer
+  field :pie, type: Integer
+  
+  # Secondary attributes
+  field :acc, type: Integer
+  field :crt, type: Integer
+  field :det, type: Integer
+  field :par, type: Integer
+  field :sks, type: Integer
+  field :sps, type: Integer
   
   embeds_many :levels, class_name: "Character::Level" do
     def for(cls)
@@ -34,10 +50,12 @@ class Character
   index "levels.cls" => 1
   
   def parse_doc(doc)
+    # General information
     name_link = doc.at_css('.player_name_txt h2 a')
     self.id = name_link['href'].split('/').last.to_i
     self.name = name_link.content
     
+    # Classes, levels and exp
     for cls_cell in doc.css('.class_list .ic_class_wh24_box') do
       next if cls_cell.content.blank?
       
@@ -55,6 +73,28 @@ class Character
       lvl.level = level
       lvl.exp = exp
       lvl.need = need
+    end
+    
+    # Primary stats
+    stat_cells = doc.css('.param_list_attributes li span')
+    [:str, :dex, :vit, :int, :mnd, :pie].each_with_index do |stat, i|
+      cell = stat_cells[i]
+      self[stat] = cell.content.to_i
+    end
+    
+    # Secondary stats
+    attrs = {
+      "Accuracy" => :acc, "Critical Hit Rate" => :crt,
+      "Determination" => :det, "Parry" => :par,
+      "Skill Speed" => :sks, "Spell Speed" => :sps,
+    }
+    doc.css('.param_list li').each do |cell|
+      name = cell.first_element_child.content
+      next unless attrs.has_key? name
+      
+      value = cell.last_element_child.content.to_i
+      sym = attrs[name]
+      self[sym] = value
     end
   end
 end
