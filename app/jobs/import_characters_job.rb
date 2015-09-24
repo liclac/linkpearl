c6f22bde064e19674122b1f90f98939390207dac
uppfinnarn <uppfinnarn@gmail.com>
2015-09-24 15:11:32 +0000

    Predecence is hard

--- app/jobs/import_characters_job.rb
+++ app/jobs/import_characters_job.rb
@@ -3,7 +3,7 @@ class ImportCharactersJob < ActiveJob::Base
 
   def perform(first_id = nil, batch_size = nil)
     first_id = first_id || Character.where(:name.ne => nil).desc(:_id).first.try(:id) || 1
-    batch_size = batch_size || Character.where(:_id.gt => first_id).count >= 50 ? 150 : 500
+    batch_size = batch_size || (Character.where(:_id.gt => first_id).count >= 50 ? 150 : 500)
     
     last_id = first_id + batch_size
     (first_id...last_id).each do |id|
