cac68a63d1de28e0033a938e3c0eb73a83604946
uppfinnarn <uppfinnarn@gmail.com>
2015-09-25 19:52:56 +0000

    GET /api/v1/characters/:id

--- app/api/api/v1/characters.rb
+++ app/api/api/v1/characters.rb
@@ -11,6 +11,19 @@ module API
           characters = Character.where(:name.ne => nil).order(:id => 1).limit(20)
           present characters, with: API::V1::Entities::Character
         end
+        
+        desc "Returns a specific character" do
+          success API::V1::Entities::Character
+          failure [
+            [404, "The character does not exist", API::V1::Entities::Error],
+          ]
+        end
+        params do
+          requires :id, type: Integer, desc: "Lodestone ID"
+        end
+        get ':id' do
+          present Character.find(params[:id]), with: API::V1::Entities::Character
+        end
       end
     end
   end
---
+++ app/api/api/v1/entities/error.rb
@@ -0,0 +1,14 @@
+module API
+  module V1
+    module Entities
+      class Error < Grape::Entity
+        expose :error, documentation: {
+          type: String, required: true, defaultValue: "Can't let you do that",
+          desc: "A short error message"
+        }
+        
+        def self.entity_name() "Error" end
+      end
+    end
+  end
+end
