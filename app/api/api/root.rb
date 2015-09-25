19d8968ed1d88f534e7172f3ff7132823603a687
uppfinnarn <uppfinnarn@gmail.com>
2015-09-25 21:46:14 +0000

    Proper errors from not founds

--- app/api/api/root.rb
+++ app/api/api/root.rb
@@ -3,6 +3,11 @@ module API
     content_type :json, 'application/json'
     default_format :json
     
+    # rescue_from :all
+    rescue_from Mongoid::Errors::DocumentNotFound do |e|
+      error!({ error: e.problem }, 404)
+    end
+    
     mount API::V1::Root
     
     add_swagger_documentation \
